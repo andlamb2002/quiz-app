5bd49ef34854cef83b256beefa7e47acc8c6b954
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-04-21 03:50:30 -0400

    Break all for questions

--- client/src/Components/Question.jsx
+++ client/src/Components/Question.jsx
@@ -3,7 +3,7 @@
 const Question = ({ question, onChange, response, showResults }) => {
 
     return (
-        <div className={`bg-bg2 text-white p-4 rounded-lg mb-4 shadow-lg mx-auto py-6`}>
+        <div className={`bg-bg2 text-white break-all p-4 rounded-lg mb-4 shadow-lg mx-auto py-6`}>
             <h4 className="text-3xl font-bold mb-2 pb-4">{question.term}</h4>
             <div className="flex flex-col text-xl space-y-2">
                 {question.options.map((option, index) => (
